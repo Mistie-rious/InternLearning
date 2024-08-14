186088d5ed6490537b4eaff149faf4dfaf39bd37
Mistie-rious <misturayahaya1@gmail.com>
2024-08-14 14:39:55 +0100

    fixed error handling

--- middlewares/errorHandler.ts
+++ middlewares/errorHandler.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 const errorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
-const code = res.statusCode ? res.statusCode : 500;
+ const code = res.statusCode !== 200 ? res.statusCode : 500;
 
 res.status(code).json({
     code,
